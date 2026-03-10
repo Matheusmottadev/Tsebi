54270b61066c215c1069890f7d0b0f175a57ccaa
Matheus <oimatheus64@gmail.com>
2026-03-10 17:21:56 -0300

    descrição rápida

---
+++ web-next/src/app/admin/layout.tsx
@@ -0,0 +1,15 @@
+﻿import type { ReactNode } from "react";
+import "@/app/globals.css";
+import styles from "./admin.module.css";
+
+type AdminLayoutProps = {
+  children: ReactNode;
+};
+
+export default function AdminLayout({ children }: AdminLayoutProps) {
+  return (
+    <div className={styles.adminRoot}>
+      <div className={styles.main}>{children}</div>
+    </div>
+  );
+}
